3af6065884cc5ae26fe12861195eaf28c7060788
REX <cssmith431@yahoo.com>
2022-03-17 14:11:00 -0400

    x

--- server.js
+++ server.js
@@ -18,6 +18,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(require('./controllers/'));
 
-sequelize.sync({ force: false }).then(() => {
+sequelize.sync({ force: false}).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
